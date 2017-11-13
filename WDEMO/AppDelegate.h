7f493028f061623abd12a121ba27833255461a93
wenchaowu <wenchaowu@pptv.com>
2017-11-13 10:58:57 +0800

    Initial Commit

---
+++ WDEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDEMO
+//
+//  Created by longzhu on 2017/11/13.
+//  Copyright © 2017年 wwc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDEMO
+//
+//  Created by longzhu on 2017/11/13.
+//  Copyright © 2017年 wwc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
