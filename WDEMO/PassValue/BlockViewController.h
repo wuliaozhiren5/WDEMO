79c6f99ed8780f01f8c21ef18cbaf5d3e74deceb
sun <wu_chaochao@126.com>
2020-08-01 22:31:10 +0800

    block typedef void(^ButtonClickBlock)(NSString * _Nonnull str);//不可以为空 typedef void(^ButtonClickBlock)(NSString * _Nullable str);//可以为空

--- WDEMO/PassValue/BlockViewController.h
+++ WDEMO/PassValue/BlockViewController.h
@@ -8,11 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 //写法1
 //typedef void(^ButtonClickBlock)(void);
-typedef void(^ButtonClickBlock)(NSString *);
-
-NS_ASSUME_NONNULL_BEGIN
+typedef void(^ButtonClickBlock)(NSString * _Nonnull str);//不可以为空
+//typedef void(^ButtonClickBlock)(NSString * _Nullable str);//可以为空
 
 @interface BlockViewController : UIViewController
 
