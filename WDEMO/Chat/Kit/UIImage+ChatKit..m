15660370e0ea3a1f7e58962f40f90dcb4207d9ea
wuwenchao <>
2020-08-07 18:03:29 +0800

    截取图片

--- WDEMO/Chat/Kit/UIImage+ChatKit..m
+++ WDEMO/Chat/Kit/UIImage+ChatKit..m
@@ -55,4 +55,26 @@
     UIImage *image = [self convertViewToImage:room];
     return image;
 }
+
+/**
+ *  从图片中按指定的位置大小截取图片的一部分
+ *
+ *  @param image UIImage image 原始的图片
+ *  @param rect  CGRect rect 要截取的区域
+ *
+ *  @return UIImage
+ */
++ (UIImage *)ct_imageFromImage:(UIImage *)image inRect:(CGRect)rect{
+    
+    //把像 素rect 转化为 点rect（如无转化则按原图像素取部分图片）
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGFloat x= rect.origin.x*scale,y=rect.origin.y*scale,w=rect.size.width*scale,h=rect.size.height*scale;
+    CGRect dianRect = CGRectMake(x, y, w, h);
+
+    //截取部分图片并生成新图片
+    CGImageRef sourceImageRef = [image CGImage];
+    CGImageRef newImageRef = CGImageCreateWithImageInRect(sourceImageRef, dianRect);
+    UIImage *newImage = [UIImage imageWithCGImage:newImageRef scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp];
+    return newImage;
+}
 @end
--- WDEMO/Chat/Kit/UIImage+ChatKit.h
+++ WDEMO/Chat/Kit/UIImage+ChatKit.h
@@ -20,6 +20,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 //房主标签图片
 + (UIImage *)roomOwnimage;
+
+/**
+*  从图片中按指定的位置大小截取图片的一部分
+*
+*  @param image UIImage image 原始的图片
+*  @param rect  CGRect rect 要截取的区域
+*
+*  @return UIImage
+*/
++ (UIImage *)ct_imageFromImage:(UIImage *)image inRect:(CGRect)rect;
+
 @end
 
 NS_ASSUME_NONNULL_END
