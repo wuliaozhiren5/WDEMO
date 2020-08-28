36e0c8c0a8b12dbe611a9b92960cb6b91d5a273e
wuwenchao <>
2020-08-28 14:24:25 +0800

    RAC 使用

---
+++ WDEMO/WDEMO/ReactiveObjC/RAC.md
@@ -0,0 +1,43 @@
+#RAC
+
+```
+//基本用法
+//iOS开发RAC学习笔记(四)RAC常用的六个用法
+//https://www.jianshu.com/p/3db561083fef
+//RAC之常用方法-----新手入门
+//https://www.cnblogs.com/crazygeek/p/6489696.html
+//RAC 代替代理、KVO、通知、事件
+//https://www.jianshu.com/p/51de7388c4b0
+
+//学习RAC小记-适合给新手看的RAC用法总结
+//https://www.jianshu.com/p/ff79a5ae0353
+//IOS RAC使用 -- ReactiveObjC
+//https://www.jianshu.com/p/5fc71f541b1c
+//iOS RAC - 基本用法
+//https://www.jianshu.com/p/cd4031fbf8ff
+```
+
+```
+/*
+//MVVM+RAC
+
+iOS MVVM+RAC
+https://www.jishudog.com/22655/html
+
+iOS开发之RAC+MVVM实战
+https://www.jianshu.com/p/f71f8a17152f
+
+【iOS开发】MVVM中使用RACCommand做网络请求
+https://cloud.tencent.com/developer/article/1328768
+*/
+```
+
+```
+//RACCommand
+//iOS开发RAC学习笔记(三)RACCommand
+//https://www.jianshu.com/p/dc472c644e7b
+
+//ReactiveCocoa(五) 之RACCommand使用
+//https://www.jianshu.com/p/59557fedfb3c
+ 
+```
--- WDEMO/WDEMO/ReactiveObjC/RACViewController.m
+++ WDEMO/WDEMO/ReactiveObjC/RACViewController.m
@@ -11,8 +11,8 @@
 
 @interface RACViewController ()
 
-@property (strong, nonatomic) UITextField *accountTF;
-@property (strong, nonatomic) UITextField *passwordTF;
+//@property (strong, nonatomic) UITextField *accountTF;
+//@property (strong, nonatomic) UITextField *passwordTF;
 @property (strong, nonatomic) UIButton *loginBtn;
 @property (strong, nonatomic) UIButton *registerBtn;
 
@@ -27,6 +27,9 @@
     [self setupViews];
     [self bindViewModel];
     //    [self loginEvent];
+    
+    
+    [self target];
 }
 - (void)setupViews {
     //    UINavigationBar与UITabBar半透明：会被遮挡；不透明，不会被遮挡
@@ -34,17 +37,17 @@
     self.view.backgroundColor = [UIColor whiteColor];
     //**************************************************************************
     
-    _accountTF = [[UITextField alloc]initWithFrame:CGRectMake(10, 10, 300, 50)];
-    _accountTF.placeholder = @"请输入账号";
-    _accountTF.borderStyle = UITextBorderStyleRoundedRect;
-    //    _accountTF.backgroundColor = [UIColor grayColor];
-    [self.view addSubview:_accountTF];
-    
-    _passwordTF = [[UITextField alloc]initWithFrame:CGRectMake(10, 70, 300, 50)];
-    _passwordTF.placeholder = @"请输入密码";
-    _passwordTF.borderStyle = UITextBorderStyleRoundedRect;
-    //    _passwordTF.backgroundColor = [UIColor grayColor];
-    [self.view addSubview:_passwordTF];
+    //    _accountTF = [[UITextField alloc]initWithFrame:CGRectMake(10, 10, 300, 50)];
+    //    _accountTF.placeholder = @"请输入账号";
+    //    _accountTF.borderStyle = UITextBorderStyleRoundedRect;
+    //    //    _accountTF.backgroundColor = [UIColor grayColor];
+    //    [self.view addSubview:_accountTF];
+    //
+    //    _passwordTF = [[UITextField alloc]initWithFrame:CGRectMake(10, 70, 300, 50)];
+    //    _passwordTF.placeholder = @"请输入密码";
+    //    _passwordTF.borderStyle = UITextBorderStyleRoundedRect;
+    //    //    _passwordTF.backgroundColor = [UIColor grayColor];
+    //    [self.view addSubview:_passwordTF];
     
     _loginBtn = [[UIButton alloc]initWithFrame:CGRectMake(10, 150, 100, 50)];
     [_loginBtn setTitle:@"login" forState:UIControlStateNormal];
@@ -61,10 +64,10 @@
 #pragma mark - ViewModel处理
 - (void)bindViewModel {
     
-//    textField输入内容的实时监听
-    [_accountTF.rac_textSignal subscribeNext:^(id x) {
-        NSLog(@"%@",x);
-    }];
+    //    textField输入内容的实时监听
+    //    [_accountTF.rac_textSignal subscribeNext:^(id x) {
+    //        NSLog(@"%@",x);
+    //    }];
     
     //
     //    //给ViewModel账号密码绑定信号
@@ -120,16 +123,97 @@
     //
     //    }];
     
-    // RAC监听按钮的点击事件
-    [[_registerBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
-        NSLog(@"点击按钮...");
-        NSLog(@"x:%@", x);
-    }];
+    //    // RAC监听按钮的点击事件
+    //    [[_registerBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+    //        NSLog(@"点击按钮...");
+    //        NSLog(@"x:%@", x);
+    //    }];
+    
+    //代替KVO
+    //    [RACObserve(self.tableView, contentOffset) subscribeNext:^(id x) {
+    //        NSLog(@"success");
+    //    }];
 }
 
 - (void)click:(UIButton *) btn{
     
 }
 
+//1 代替代理:
+- (void)delegate {
+    
+    [_loginBtn addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [[self rac_signalForSelector:@selector(click:)] subscribeNext:^(RACTuple * x) {
+        NSLog(@"点击了");
+        NSLog(@"%@,%@",x.first,x.second);
+    }];
+    
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"RAC" message:@"RAC TEST" delegate:self cancelButtonTitle:@"cancel" otherButtonTitles:@"other", nil];
+    [[self rac_signalForSelector:@selector(alertView:clickedButtonAtIndex:) fromProtocol:@protocol(UIAlertViewDelegate)] subscribeNext:^(RACTuple *tuple) {
+        NSLog(@"%@",tuple.first);
+        NSLog(@"%@",tuple.second);
+        NSLog(@"%@",tuple.third);
+    }];
+    [alertView show];
+    
+    [[alertView rac_buttonClickedSignal] subscribeNext:^(id x) {
+        NSLog(@"%@",x);
+    }];
+    
+    
+}
+//2 代替KVO:
+- (void)kvo {
+    
+    UIScrollView *scrolView = [[UIScrollView alloc] initWithFrame:CGRectMake(100, 10, 200, 400)];
+    scrolView.contentSize = CGSizeMake(200, 800);
+    scrolView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:scrolView];
+    [RACObserve(scrolView, contentOffset) subscribeNext:^(id x) {
+        NSLog(@"success");
+    }];
+}
+//4 代替通知:
+//3 监听事件:
+- (void)target {
+    
+    // 把按钮点击事件转换为信号，点击按钮，就会发送信号
+    [[_registerBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+        NSLog(@"按钮被点击了");
+        NSLog(@"点击按钮...");
+        NSLog(@"x:%@", x);
+    }];
+    
+    //给Lable或者View添加监听
+    UILabel *textLab = [[UILabel alloc] initWithFrame:CGRectMake(100, 250, 200, 30)];
+    textLab.backgroundColor = [UIColor greenColor];
+    textLab.text = @"********************";
+    textLab.userInteractionEnabled = YES;
+    [self.view addSubview:textLab];
+    
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] init];
+    [tap.rac_gestureSignal subscribeNext:^(id x) {
+        NSLog(@"点击Lab");
+    }];
+    [textLab addGestureRecognizer:tap];
+}
 
+//5 监听文本框文字改变:
+- (void)text {
+    UITextField *text1 = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, 150, 50)];
+    text1.backgroundColor = [UIColor cyanColor];
+    text1.placeholder = @"请输入账号";
+    text1.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:text1];
+    
+    [[text1 rac_textSignal] subscribeNext:^(id x) {
+        static int i = 0;
+        i++;
+        //  text改变就会打印
+        NSLog(@"厉害了我的哥------ %d", i);
+    }];
+}
 @end
