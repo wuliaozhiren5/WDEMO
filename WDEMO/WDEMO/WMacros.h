04cf237ca3263e18dfd1e02f15d5ed2532aaa186
wuwenchao <>
2020-10-12 11:43:03 +0800

    宏定义 WeakSelf(obj)  StrongSelf(obj)

--- WDEMO/WDEMO/WMacros.h
+++ WDEMO/WDEMO/WMacros.h
@@ -32,8 +32,23 @@ dispatch_async(dispatch_get_main_queue(), block);\
 //weakSelf,strongSelf
 //__weak __typeof(self) weakSelf = self;
 //__strong __typeof(self) strongSelf = weakSelf;
+//__strong __typeof(weakSelf) strongSelf = weakSelf;
+
 #define WeakObject(obj) __weak typeof(obj) weakObject = obj;
 #define StrongObject(obj) __strong typeof(obj) strongObject = weakObject;
+ 
+#define WeakSelf(obj) __weak typeof(obj) weakSelf = obj;
+#define StrongSelf(obj) __strong typeof(obj) strongSelf = weakSelf;
+
+//#define WEAKSELF typeof(self) __weak weakSelf = self;
+//#define STRONGSELF typeof(weakSelf) __strong strongSelf = weakSelf;
+
+
+#define WS(weakSelf)  __weak __typeof(&*self)weakSelf = self;
+//用法如下：
+//WS(weakself)
+//...
+//[weakSelf doSomething];
 
 #endif /* WMacros_h */
 
