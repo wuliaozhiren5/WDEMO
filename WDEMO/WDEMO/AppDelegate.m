26622fb040fde26d34211a72dc71f31365b998e1
wuwenchao <>
2020-10-15 15:02:49 +0800

    简单的  TabBarController

--- WDEMO/WDEMO/AppDelegate.m
+++ WDEMO/WDEMO/AppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "AppDelegate.h"
 #import "CJAppUncaughtExceptionHandler.h"
+#import "AppNavigationController.h"
+#import "AppTabBarController.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -31,8 +34,70 @@
     
     //code...
     //调用自定义类中的收集崩溃信息的方法
-//    [CJAppUncaughtExceptionHandler InstallUncaughtExceptionHandler];
+    //    [CJAppUncaughtExceptionHandler InstallUncaughtExceptionHandler];
     //code...
+    
+    //    UITabBarController的使用详解及其自定义
+    //    https://www.jianshu.com/p/2f74a5d93faa
+ 
+    // 创建窗口
+    self.window = [[UIWindow alloc]init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    
+    
+    // 设置窗口的跟控制器
+    AppTabBarController * tabbarVC = [[AppTabBarController alloc]init];
+    
+    // 添加子控制器
+//    UIViewController * VC01 = [[UIViewController alloc]init];
+    ViewController * VC01 = [[ViewController alloc]init];
+    // 添加子导航控制器
+    AppNavigationController * NavVC01 = [[AppNavigationController alloc]initWithRootViewController:VC01];
+    // 设置标题
+    NavVC01.tabBarItem.title = @"精华";
+    // 设置默认图片
+    NavVC01.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
+    // 设置选中图片
+    NavVC01.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
+    NavVC01.view.backgroundColor = [UIColor yellowColor];
+    [tabbarVC addChildViewController:NavVC01];
+    
+    UIViewController * VC02 = [[UIViewController alloc]init];
+    AppNavigationController * NavVC02 = [[AppNavigationController alloc]initWithRootViewController:VC02];
+    NavVC02.tabBarItem.title = @"新帖";
+    NavVC02.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
+    NavVC02.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
+    
+    NavVC02.view.backgroundColor = [UIColor redColor];
+    [tabbarVC addChildViewController:NavVC02];
+    
+    UIViewController * VC03 = [[UIViewController alloc]init];
+    AppNavigationController * NavVC03 = [[AppNavigationController alloc]initWithRootViewController:VC03];
+    NavVC03.tabBarItem.title = @"关注";
+    NavVC03.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
+    NavVC03.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
+    
+    NavVC03.view.backgroundColor = [UIColor blueColor];
+    [tabbarVC addChildViewController:NavVC03];
+    
+    UIViewController * VC04 = [[UIViewController alloc]init];
+    AppNavigationController * NavVC04 = [[AppNavigationController alloc]initWithRootViewController:VC04];
+    NavVC04.tabBarItem.title = @"我";
+    NavVC04.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
+    NavVC04.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
+    
+    NavVC04.view.backgroundColor = [UIColor greenColor];
+    [tabbarVC addChildViewController:NavVC04];
+    
+    
+    
+    self.window.rootViewController = tabbarVC;
+    
+    // 显示窗口
+    [self.window makeKeyAndVisible];
+    
+
+
     
     return YES;
 }
@@ -76,7 +141,7 @@
     }
     
 }
- 
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
