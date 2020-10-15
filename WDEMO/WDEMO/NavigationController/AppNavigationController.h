69cfaab70b26247115dfa352792937518a653be9
wuwenchao <>
2020-10-15 11:45:03 +0800

    add file

---
+++ WDEMO/WDEMO/NavigationController/AppNavigationController.h
@@ -0,0 +1,17 @@
+//
+//  AppNavigationController.h
+//  WDEMO
+//
+//  Created by rrtv on 2020/10/15.
+//  Copyright © 2020 wwc. All rights reserved.
+//
+
+#import "BaseNavigationController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppNavigationController : BaseNavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
