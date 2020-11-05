1455348e425832133480b48f2f55003db362ca65
wuwenchao <>
2020-11-05 15:49:01 +0800

    ui修改

--- WDEMO/WDEMO/ViewController.m
+++ WDEMO/WDEMO/ViewController.m
@@ -76,12 +76,18 @@
 @end
 
 @implementation ViewController
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"首页";
-//    [self clickChat];
-    [self clickWebView];
+    [self clickChat];
+//    [self clickWebView];
     //    iOS7之后由于navigationBar.translucent默认是YES，坐标零点默认在（0，0）点  当不透明的时候，零点坐标在（0，64）；如果你想设置成透明的，而且还要零点从（0，64）开始，那就添加：self.edgesForExtendedLayout = UIRectEdgeNone;
     self.navigationController.navigationBar.translucent = NO;
     //    self.edgesForExtendedLayout = UIRectEdgeNone;
@@ -286,8 +292,8 @@
 }
 
 -(void)clickChat {
-    
     ChatViewController *vc= [[ChatViewController alloc]init];
+    vc.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:vc animated:YES];
 }
 
