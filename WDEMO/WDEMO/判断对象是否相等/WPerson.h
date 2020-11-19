a46d3e69c465b7d3cca4523476ffb111b0411ade
wuwenchao <>
2020-11-19 15:33:58 +0800

    isEqual与hash

---
+++ WDEMO/WDEMO/判断对象是否相等/WPerson.h
@@ -0,0 +1,17 @@
+//
+//  WPerson.h
+//  WDEMO
+//
+//  Created by rrtv on 2020/11/19.
+//  Copyright © 2020 wwc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WDEMO/WDEMO/判断对象是否相等/WPerson.m
@@ -0,0 +1,13 @@
+//
+//  WPerson.m
+//  WDEMO
+//
+//  Created by rrtv on 2020/11/19.
+//  Copyright © 2020 wwc. All rights reserved.
+//
+
+#import "WPerson.h"
+
+@implementation WPerson
+
+@end
