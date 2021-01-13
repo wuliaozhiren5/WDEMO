a7fb8642cab494f87f3ae196b86a3b55c03e92f6
wuwenchao <>
2021-01-13 11:38:20 +0800

    new 一个waterLayout

---
+++ WDEMO/WDEMO/瀑布流布局/RRWaterLayout.h
@@ -0,0 +1,17 @@
+//
+//  RRWaterLayout.h
+//  WDEMO
+//
+//  Created by rrtv on 2021/1/13.
+//  Copyright © 2021 wwc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RRWaterLayout : UICollectionViewLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WDEMO/WDEMO/瀑布流布局/RRWaterLayout.m
@@ -0,0 +1,13 @@
+//
+//  RRWaterLayout.m
+//  WDEMO
+//
+//  Created by rrtv on 2021/1/13.
+//  Copyright © 2021 wwc. All rights reserved.
+//
+
+#import "RRWaterLayout.h"
+
+@implementation RRWaterLayout
+
+@end
