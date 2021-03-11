83b7a2e24842606d582ab5045631c9a37f0c5d20
wuwenchao <>
2021-03-11 20:50:33 +0800

    下拉图片放大，上拉显示导航栏

--- WDEMO/WDEMO/个人主页/PersonalHomepageVC.m
+++ WDEMO/WDEMO/个人主页/PersonalHomepageVC.m
@@ -25,6 +25,10 @@ static CGFloat const headViewHeight = 256;
 
 @property(nonatomic,strong) UIScrollView * parentScrollView;
 
+//header 头
+@property(nonatomic,strong)UIImageView *headImageView;//头部图片
+@property(nonatomic,strong)UIImageView *avatarImage;
+@property(nonatomic,strong)UILabel *countentLabel;
 /*
  * canScroll= yes : mainTableView 视图可以滚动，parentScrollView 禁止滚动
  */
@@ -33,6 +37,9 @@ static CGFloat const headViewHeight = 256;
 @property (nonatomic, assign) BOOL isTopIsCanNotMoveMainTableView;//到顶部不能滚动了
 
 @property (nonatomic, assign) BOOL isTopIsCanNotMoveParentScrollView;//到顶部不能滚动了
+
+@property (nonatomic, assign) CGFloat statusbarHeight;
+@property (nonatomic, assign) CGFloat navigationbarHeight;
 @end
 
 @implementation PersonalHomepageVC
@@ -40,61 +47,127 @@ static CGFloat const headViewHeight = 256;
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-
+    
     self.title = @"PersonalHomepageVC";
     
     [self.view addSubview:self.mainTableView];
-
-//    [self.mainTableView addSubview:self.headImageView];
+    
+    [self.mainTableView addSubview:self.headImageView];
     //支持下刷新。关闭弹簧效果
-//    self.mainTableView.bounces =  NO;
- 
+    //    self.mainTableView.bounces =  NO;
+    
     if (@available(iOS 11.0, *)) {
         self.mainTableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
     }else {
         self.automaticallyAdjustsScrollViewInsets = NO;
     }
     
-//    self.canScroll = YES;
+    
+    self.navigationController.navigationBar.translucent = YES;//这个必须设置
+    
+    ////////////////////
+    
+    NSLog(@"statusbar height: %f", [self getStatusBarHight]); // 高度
+    
+    
+    //    状态栏高度：20px，导航栏高度：40px，代码如下：
+    // (statusbar)
+    CGRect rectOfStatusbar = [[UIApplication sharedApplication] statusBarFrame];
+    NSLog(@"statusbar height: %f", rectOfStatusbar.size.height); // 高度
+    
+    //（navigationbar）
+    CGRect rectOfNavigationbar = self.navigationController.navigationBar.frame;
+    NSLog(@"navigationbar height: %f", rectOfNavigationbar.size.height); // 高度
+    
+    //    iOS：状态栏和导航栏的高度获取
+    //    作者：柠檬丶Lemon
+    //    链接：https://www.jianshu.com/p/9064d8103022
+    //    来源：简书
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    
+    //    iOS 13后状态栏获取高度的方法
+    //    作者：li125434104
+    //    链接：https://www.jianshu.com/p/d60757f13038
+    //    来源：简书
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    
+    self.statusbarHeight = [self getStatusBarHight];
+    self.navigationbarHeight = self.navigationController.navigationBar.frame.size.height;
+}
+
+//iOS 10、设置导航栏全透明
+//https://www.jianshu.com/p/e97086eb1f15
+//iOS轻松实现导航栏透明渐变
+//https://www.cnblogs.com/Lovexiaohuzi/p/6866351.html
+//iOS轻松实现导航栏透明渐变
+//https://www.jb51.net/article/103964.htm
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [super viewWillAppear:animated];
+    //设置导航栏背景图片为一个空的image，这样就透明了
+    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init] forBarMetrics:UIBarMetricsDefault];
+    
+    //去掉透明后导航栏下边的黑边
+    [self.navigationController.navigationBar setShadowImage:[[UIImage alloc] init]];
+    self.navigationController.navigationBar.translucent = YES;//这个必须设置
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    //    如果不想让其他页面的导航栏变为透明 需要重置
+    [self.navigationController.navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setShadowImage:nil];
+}
+
+- (CGFloat)getStatusBarHight {
+    float statusBarHeight = 0;
+    if (@available(iOS 13.0, *)) {
+        UIStatusBarManager *statusBarManager = [UIApplication sharedApplication].windows.firstObject.windowScene.statusBarManager;
+        statusBarHeight = statusBarManager.statusBarFrame.size.height;
+    }
+    else {
+        statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+    }
+    return statusBarHeight;
 }
 
 #pragma mark -- scrollDelegate
 -(void)scrollDidScroll:(UIScrollView *)scrollView {
     
     self.parentScrollView = scrollView;
-
+    
     CGFloat offsetY = scrollView.contentOffset.y;
-//    if (offsetY > headViewHeight) {
-//
-//    }
+    //    if (offsetY > headViewHeight) {
+    //
+    //    }
     
-//    self.parentScrollView.contentOffset = CGPointMake(0, 0);
-      
-//    if (self.isTopIsCanNotMoveMainTableView) {
-//        self.parentScrollView.contentOffset = CGPointMake(0, 0);
-//    } else {
-//        self.parentScrollView.contentOffset = CGPointMake(0, 0);
-//    }
+    //    self.parentScrollView.contentOffset = CGPointMake(0, 0);
     
-//    CGPoint translatedPoint = [scrollView.panGestureRecognizer translationInView:scrollView];
-//    if(translatedPoint.y < 0) {
-//        NSLog(@"上滑");
-//        
-//        if (self.isTopIsCanNotMoveMainTableView) {
-//            self.isTopIsCanNotMoveParentScrollView = YES;
-//        } else {
-//            self.parentScrollView.contentOffset = CGPointMake(0, 0);
-//        }
-//    }
-//        
-//    if(translatedPoint.y > 0) {
-//        NSLog(@"下滑");
-//        if (offsetY < 0) {
-//            
-//        } else {
-//            
-//        } 
-//    }
+    //    if (self.isTopIsCanNotMoveMainTableView) {
+    //        self.parentScrollView.contentOffset = CGPointMake(0, 0);
+    //    } else {
+    //        self.parentScrollView.contentOffset = CGPointMake(0, 0);
+    //    }
+    
+    //    CGPoint translatedPoint = [scrollView.panGestureRecognizer translationInView:scrollView];
+    //    if(translatedPoint.y < 0) {
+    //        NSLog(@"上滑");
+    //
+    //        if (self.isTopIsCanNotMoveMainTableView) {
+    //            self.isTopIsCanNotMoveParentScrollView = YES;
+    //        } else {
+    //            self.parentScrollView.contentOffset = CGPointMake(0, 0);
+    //        }
+    //    }
+    //
+    //    if(translatedPoint.y > 0) {
+    //        NSLog(@"下滑");
+    //        if (offsetY < 0) {
+    //
+    //        } else {
+    //
+    //        }
+    //    }
     
     if (offsetY < 0) {
         //离开顶部 主View 可以滑动
@@ -186,81 +259,85 @@ static CGFloat const headViewHeight = 256;
     /*
      *  处理联动事件
      */
-
+    
     //获取滚动视图y值的偏移量
-    CGFloat tabOffsetY = 0;
+    CGFloat tabOffsetY = - self.navigationbarHeight - self.statusbarHeight;
     CGFloat offsetY = scrollView.contentOffset.y;
-
+    
     if (offsetY >= headViewHeight) {
         scrollView.contentOffset = CGPointMake(0, tabOffsetY);
         offsetY = tabOffsetY;
     }
-     
-//    self.isTopIsCanNotMoveParentScrollView = self.isTopIsCanNotMoveMainTableView;
- 
+    
+    //    self.isTopIsCanNotMoveParentScrollView = self.isTopIsCanNotMoveMainTableView;
+    
     if (!self.isTopIsCanNotMoveParentScrollView) {
         if (offsetY >= tabOffsetY) {
             scrollView.contentOffset = CGPointMake(0, tabOffsetY);
             self.isTopIsCanNotMoveMainTableView = YES;
             
-    //        self.canScroll = NO;
+            //        self.canScroll = NO;
         }else{
             self.isTopIsCanNotMoveMainTableView = NO;
-//            self.isTopIsCanNotMoveParentScrollView = NO;
-
-    //        self.canScroll = YES;
-    //        self.parentScrollView.contentOffset = CGPointMake(0, 0);
+            //            self.isTopIsCanNotMoveParentScrollView = NO;
+            
+            //        self.canScroll = YES;
+            //        self.parentScrollView.contentOffset = CGPointMake(0, 0);
         }
     } else {
         scrollView.contentOffset = CGPointMake(0, tabOffsetY);
     }
-     
-  
-//    if (self.isTopIsCanNotMoveMainTableView != self.isTopIsCanNotMoveParentScrollView) {
-//        if (!self.isTopIsCanNotMoveParentScrollView && self.isTopIsCanNotMoveMainTableView) {
-//            //滑动到顶端
-//            self.canScroll = NO;
-//        }
-//
-//        if(self.isTopIsCanNotMoveParentScrollView && !self.isTopIsCanNotMoveMainTableView){
-//            //离开顶端
-//            if (!self.canScroll) {
-//                scrollView.contentOffset = CGPointMake(0, tabOffsetY);
-//            }else{
-//                self.parentScrollView.contentOffset = CGPointMake(0, tabOffsetY);
-//            }
-//        }
-//    }else
-//    {
-//        if (!self.canScroll){
-//             //支持下刷新,下拉时maintableView 没有滚动到位置 parentScrollView 不进行刷新
-//            CGFloat parentScrollViewOffsetY = self.parentScrollView.contentOffset.y;
-//            if(parentScrollViewOffsetY >0)
-//                self.parentScrollView.contentOffset = CGPointMake(0, 0);
-//        }else
-//        {
-//            self.parentScrollView.contentOffset = CGPointMake(0, 0);
-//        }
-//    }
+    
+    
+    //    if (self.isTopIsCanNotMoveMainTableView != self.isTopIsCanNotMoveParentScrollView) {
+    //        if (!self.isTopIsCanNotMoveParentScrollView && self.isTopIsCanNotMoveMainTableView) {
+    //            //滑动到顶端
+    //            self.canScroll = NO;
+    //        }
+    //
+    //        if(self.isTopIsCanNotMoveParentScrollView && !self.isTopIsCanNotMoveMainTableView){
+    //            //离开顶端
+    //            if (!self.canScroll) {
+    //                scrollView.contentOffset = CGPointMake(0, tabOffsetY);
+    //            }else{
+    //                self.parentScrollView.contentOffset = CGPointMake(0, tabOffsetY);
+    //            }
+    //        }
+    //    }else
+    //    {
+    //        if (!self.canScroll){
+    //             //支持下刷新,下拉时maintableView 没有滚动到位置 parentScrollView 不进行刷新
+    //            CGFloat parentScrollViewOffsetY = self.parentScrollView.contentOffset.y;
+    //            if(parentScrollViewOffsetY >0)
+    //                self.parentScrollView.contentOffset = CGPointMake(0, 0);
+    //        }else
+    //        {
+    //            self.parentScrollView.contentOffset = CGPointMake(0, 0);
+    //        }
+    //    }
     
     
     /**
      * 处理头部视图
      */
-//    CGFloat yOffset  = scrollView.contentOffset.y;
-//    if(yOffset < -headViewHeight) {
-//
-//        CGRect f = self.headImageView.frame;
-//        f.origin.y= yOffset ;
-//        f.size.height=  -yOffset;
-//        f.origin.y= yOffset;
-//
-//        //改变头部视图的fram
-//        self.headImageView.frame= f;
-//        CGRect avatarF = CGRectMake(f.size.width/2-40, (f.size.height-headViewHeight)+56, 80, 80);
-//        _avatarImage.frame = avatarF;
-//        _countentLabel.frame = CGRectMake((f.size.width-Main_Screen_Width)/2+40, (f.size.height-headViewHeight)+172, Main_Screen_Width-80, 36);
-//    }
+    CGFloat yOffset  = scrollView.contentOffset.y;
+    if(yOffset < -headViewHeight) {
+        
+        CGRect f = self.headImageView.frame;
+        f.origin.y= yOffset ;
+        f.size.height=  -yOffset;
+        f.origin.y= yOffset;
+        
+        //改变头部视图的fram
+        self.headImageView.frame= f;
+        CGRect avatarF = CGRectMake(f.size.width/2-40, (f.size.height-headViewHeight)+56, 80, 80);
+        _avatarImage.frame = avatarF;
+        _countentLabel.frame = CGRectMake((f.size.width-Screen_Width)/2+40, (f.size.height-headViewHeight)+172, Screen_Width-80, 36);
+    } else {
+        
+        //透明度
+        self.headImageView.alpha = 1+yOffset/headViewHeight;
+    }
 }
 
 #pragma mark --tableDelegate
@@ -273,8 +350,8 @@ static CGFloat const headViewHeight = 256;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-     
-    return Screen_Height - 94;//Screen_Height - 94
+    
+    return Screen_Height - self.navigationbarHeight - self.statusbarHeight;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -292,14 +369,14 @@ static CGFloat const headViewHeight = 256;
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 #pragma mark -- setter/getter
 
 -(UIView *)setPageViewControllers
@@ -320,15 +397,15 @@ static CGFloat const headViewHeight = 256;
     twoVc.delegate = self;
     ThirdViewCollectionViewController * thirdVc  = [ThirdViewCollectionViewController new];
     thirdVc.delegate = self;
-     
+    
     NSArray *viewControllers = @[oneVc,twoVc,thirdVc];
     NSArray *titles = @[@"first",@"second",@"third"];
- 
+    
     HomePageViewController *pageVC= [[HomePageViewController alloc] initWithViewControllerClasses:viewControllers andTheirTitles:titles];
     pageVC.viewControllers = viewControllers;
     
     pageVC.selectIndex = 0;
-//    pageVC.title = @"首页样式";
+    //    pageVC.title = @"首页样式";
     //带下划线
     pageVC.menuViewStyle = WMMenuViewStyleLine;
     ///** 是否自动通过字符串计算 MenuItem 的宽度，默认为 NO. */
@@ -342,9 +419,9 @@ static CGFloat const headViewHeight = 256;
     //底部小横线样式，小红线长度
     pageVC.progressViewIsNaughty = YES;
     pageVC.progressWidth = 10;
-//        vc.selectIndex = 1;
-//        vc.automaticallyCalculatesItemWidths = YES;
-//        vc.titleSizeSelected = 16;
+    //        vc.selectIndex = 1;
+    //        vc.automaticallyCalculatesItemWidths = YES;
+    //        vc.titleSizeSelected = 16;
     
     return pageVC;
     
@@ -354,9 +431,42 @@ static CGFloat const headViewHeight = 256;
     NSLog(@"%@",viewController);
 }
 
+
+#pragma mark -- lazy 懒加载
+-(UIImageView *)headImageView
+{
+    if (_headImageView == nil)
+    {
+        _headImageView= [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"bg.jpg"]];
+        _headImageView.frame=CGRectMake(0, -headViewHeight ,Screen_Width,headViewHeight);
+        _headImageView.userInteractionEnabled = YES;
+        _headImageView.contentMode = UIViewContentModeScaleAspectFill;
+        
+        _avatarImage = [[UIImageView alloc] initWithFrame:CGRectMake(Screen_Width/2-40, 56, 80, 80)];
+        [_headImageView addSubview:_avatarImage];
+        _avatarImage.userInteractionEnabled = YES;
+        _avatarImage.layer.masksToBounds = YES;
+        _avatarImage.layer.borderWidth = 1;
+        _avatarImage.layer.borderColor =[[UIColor colorWithRed:255/255. green:253/255. blue:253/255. alpha:1.] CGColor];
+        _avatarImage.layer.cornerRadius = 40;
+        _avatarImage.image = [UIImage imageNamed:@"avatar.jpg"];
+        
+        
+        _countentLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 150, Screen_Width-80, 30)];
+        _countentLabel.font = [UIFont systemFontOfSize:12.];
+        _countentLabel.textColor = [UIColor colorWithRed:255/255. green:255/255. blue:255/255. alpha:1.];
+        _countentLabel.textAlignment = NSTextAlignmentCenter;
+        _countentLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        _countentLabel.numberOfLines = 0;
+        _countentLabel.text = @"我的名字叫Anna";
+        [_headImageView addSubview:_countentLabel];
+    }
+    return _headImageView;
+}
+
 - (MainTouchTableTableView *)mainTableView {
     if (_mainTableView == nil) {
-        _mainTableView= [[MainTouchTableTableView alloc]initWithFrame:CGRectMake(0,0, Screen_Width,Screen_Height - 94)];
+        _mainTableView= [[MainTouchTableTableView alloc]initWithFrame:CGRectMake(0,0, Screen_Width,Screen_Height)];
         _mainTableView.delegate = self;
         _mainTableView.dataSource = self;
         _mainTableView.showsVerticalScrollIndicator = NO;
