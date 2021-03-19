07719161da78da389014643923e6c5e7bffd316d
wuwenchao <>
2021-03-19 14:25:50 +0800

    BUG： 文字显示错误，演员介绍因该3行 ，但是只展示了两行

--- WDEMO/WDEMO/Actor/RRActorInfoCell.m
+++ WDEMO/WDEMO/Actor/RRActorInfoCell.m
@@ -42,16 +42,16 @@
         make.width.equalTo(@(60));
         make.height.equalTo(@(28));
     }];
-    
-
 }
 
 - (void)setModel:(id)model {
+    _model = model;
     //最大69
     
     UIFont *textFont = self.titleLab.font;
     //    UIFont *textFont = [UIFont systemFontOfSize:16];
-    NSString *contentStr = @"阿斯顿就离开撒的饭卡上阿斯顿就离开撒的ref 被用来给DOM元素或子组件注册引用信息。引用信息会根据父组件的 $refs 对象进行注册。如果在普通的DOM元素上使用，引用信息就是元素; 如果用在子组件上，引用信息就是组件实例.注意：只要想要在Vue中直接操作DOM元素，就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册";
+    //    NSString *contentStr = model.desc;
+    NSString *contentStr= @"阿斯顿就离开撒的饭卡上阿斯顿就离开撒的ref 被用来给DOM元素或子组件注册引用信息。引用信息会根据父组件的 $refs 对象进行注册。如果在普通的DOM元素上使用，引用信息就是元素; 如果用在子组件上，引用信息就是组件实例.注意：只要想要在Vue中直接操作DOM元素，就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册";
     NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:contentStr];
     NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc]init];
     paragraph.lineSpacing = 9 - (textFont.lineHeight - textFont.pointSize);
@@ -59,51 +59,47 @@
                                       //                                      NSForegroundColorAttributeName: [UIColor blackColor],
                                       NSParagraphStyleAttributeName:paragraph}
                               range:NSMakeRange(0, attributedString.length)];
+    
     self.titleLab.attributedText = attributedString;
     self.titleLab.lineBreakMode = NSLineBreakByTruncatingTail;
-    
-    //计算高度
-    CGRect rect = [attributedString boundingRectWithSize:CGSizeMake(KWidth - 16 * 2, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
-    CGSize size  = rect.size;
-    CGFloat height = size.height;//真实的高度
-    CGFloat showHeight = height;//显示的高度
-    
-    
-//    if (self.isNoOtherData) {
-//        self.isShowMore = YES;
-//    } else {
-//        
-//    }
-    
-    //按钮
-    self.moreBtn.hidden = NO;
-    if (height <= 66) {
-        //不显示more按钮
-        self.moreBtn.hidden = YES;
-    } else {
-        //显示more按钮
-        self.moreBtn.hidden = NO;
-        showHeight = 66;
-    }
-    
-    //状态发生变化 显示全部
     if (self.isShowMore) {
-        showHeight = height;
+        self.titleLab.numberOfLines = 0;
+    } else {
+        self.titleLab.numberOfLines = 3;
     }
-    self.moreBtn.selected = self.isShowMore;
     
-    [self.titleLab mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.height.equalTo(@(showHeight));
-    }];
+    UILabel *moreLineLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, KWidth - 16 * 2, 20)];
+    moreLineLab.numberOfLines = 0;
+    moreLineLab.attributedText = attributedString;
+    //计算高度方法
+    CGSize moreLineSize = [moreLineLab sizeThatFits:CGSizeMake(KWidth - 16 * 2, MAXFLOAT)];
+    CGFloat moreLineHeight = moreLineSize.height;
+    //    NSLog(@"计算高度方法1：text width = %f, text height = %f", size.width, size.height);
+    
+    UILabel *threeLineLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, KWidth - 16 * 2, 20)];
+    threeLineLab.numberOfLines = 3;
+    threeLineLab.attributedText = attributedString;
+    //计算高度方法
+    CGSize threeLineSize = [threeLineLab sizeThatFits:CGSizeMake(KWidth - 16 * 2, MAXFLOAT)];
+    CGFloat threeLineHeight = threeLineSize.height;
+    //    NSLog(@"计算高度方法1：text width = %f, text height = %f", size.width, size.height);
+    
+    BOOL isHidden = NO;
+    if (!(threeLineHeight < moreLineHeight)) {
+        isHidden = YES;
+    }
+    //按钮
+    self.moreBtn.hidden = isHidden;
+    self.moreBtn.selected = self.isShowMore;
 }
-  
+
 - (UILabel *)titleLab {
     if (!_titleLab) {
         _titleLab = [[UILabel alloc] init];
         _titleLab.frame = CGRectMake(0, 0, 40, 40);
         _titleLab.font = RR_COMMONFONT(14);
         _titleLab.textColor = kCOLOR_dynamicProvider_222222_E5E7EB;
-        _titleLab.numberOfLines = 0;
+        _titleLab.numberOfLines = 3;
         _titleLab.lineBreakMode = NSLineBreakByTruncatingTail;
     }
     return _titleLab;
@@ -113,20 +109,15 @@
     if (!_moreBtn) {
         _moreBtn = [[UIButton alloc] init];
         _moreBtn.frame = CGRectMake(0, 0, 40, 40);
-        //        _moreBtn.titleLabel.font = RR_COMMONFONT(12);
-        //        [_moreBtn setTitle:@"查看作品" forState:UIControlStateNormal];
-        //        [_moreBtn setTitleColor:kCOLOR_1890FF forState:UIControlStateNormal];
-        //        _moreBtn.backgroundColor = [kCOLOR_1890FF colorWithAlphaComponent:0.1];
         [_moreBtn setImage:IMAGENAME(@"ic_more_down_gray24") forState:UIControlStateNormal];
         [_moreBtn setImage:IMAGENAME(@"ic_more_up_gray24") forState:UIControlStateSelected];
-//        [_moreBtn addTarget:self action:@selector(clickMoreBtn:) forControlEvents:(UIControlEventTouchUpInside)];
     }
     return _moreBtn;
 }
- 
+
 + (CGFloat)cellHeightWithModel:(id)model {
     UIFont *textFont = RR_COMMONFONT(14);
-//    NSString *contentStr = model.desc;
+    //    NSString *contentStr = model.desc;
     NSString *contentStr= @"阿斯顿就离开撒的饭卡上阿斯顿就离开撒的ref 被用来给DOM元素或子组件注册引用信息。引用信息会根据父组件的 $refs 对象进行注册。如果在普通的DOM元素上使用，引用信息就是元素; 如果用在子组件上，引用信息就是组件实例.注意：只要想要在Vue中直接操作DOM元素，就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册";
     NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:contentStr];
     NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc]init];
@@ -135,46 +126,64 @@
                                       //                                      NSForegroundColorAttributeName: [UIColor blackColor],
                                       NSParagraphStyleAttributeName:paragraph}
                               range:NSMakeRange(0, attributedString.length)];
-    //计算高度
-    CGRect rect = [attributedString boundingRectWithSize:CGSizeMake(KWidth - 16 * 2, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
-    CGSize size  = rect.size;
-    CGFloat height = size.height;//真实的高度
-//    CGFloat showHeight = height;//显示的高度
-    
-//    //按钮  28
-////    self.moreBtn.hidden = NO;
-//    if (height <= 69) {
-//        //不显示more按钮                16
-////        self.moreBtn.hidden = YES;
-//    } else {
-//        //显示more按钮                  36
-////        self.moreBtn.hidden = NO;
-//        showHeight = 69;
-//    }
-//
-//    //状态发生变化 显示全部
-//    if (self.isShowMore) {
-//        showHeight = height;
-//    }
-    
+    UILabel *titleLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, KWidth - 16 * 2, 20)];
+    titleLab.numberOfLines = 0;
+    titleLab.attributedText = attributedString;
+    //计算高度方法
+    CGSize size = [titleLab sizeThatFits:CGSizeMake(KWidth - 16 * 2, MAXFLOAT)];
+    CGFloat height = size.height;
+    NSLog(@"计算高度方法1：text width = %f, text height = %f", size.width, size.height);
     return height;
 }
 
 + (CGFloat)cellHeightWithModel:(id)model isShowMore:(BOOL)isShowMore {
-    CGFloat height = [self cellHeightWithModel:model];
-    CGFloat showHeight = height;//显示的高度
-    //按钮  28
-    if (height <= 66) {
-        //不显示more按钮                16
+    UIFont *textFont = RR_COMMONFONT(14);
+    //    NSString *contentStr = model.desc;
+    NSString *contentStr= @"阿斯顿就离开撒的饭卡上阿斯顿就离开撒的ref 被用来给DOM元素或子组件注册引用信息。引用信息会根据父组件的 $refs 对象进行注册。如果在普通的DOM元素上使用，引用信息就是元素; 如果用在子组件上，引用信息就是组件实例.注意：只要想要在Vue中直接操作DOM元素，就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册就必须用ref属性进行注册";
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:contentStr];
+    NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc]init];
+    paragraph.lineSpacing = 9 - (textFont.lineHeight - textFont.pointSize);
+    [attributedString addAttributes:@{NSFontAttributeName:textFont,
+                                      //                                      NSForegroundColorAttributeName: [UIColor blackColor],
+                                      NSParagraphStyleAttributeName:paragraph}
+                              range:NSMakeRange(0, attributedString.length)];
+    
+    UILabel *moreLineLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, KWidth - 16 * 2, 20)];
+    moreLineLab.numberOfLines = 0;
+    moreLineLab.attributedText = attributedString;
+    //计算高度方法
+    CGSize moreLineSize = [moreLineLab sizeThatFits:CGSizeMake(KWidth - 16 * 2, MAXFLOAT)];
+    CGFloat moreLineHeight = moreLineSize.height;
+    //    NSLog(@"计算高度方法1：text width = %f, text height = %f", size.width, size.height);
+    
+    UILabel *threeLineLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, KWidth - 16 * 2, 20)];
+    threeLineLab.numberOfLines = 3;
+    threeLineLab.attributedText = attributedString;
+    //计算高度方法
+    CGSize threeLineSize = [threeLineLab sizeThatFits:CGSizeMake(KWidth - 16 * 2, MAXFLOAT)];
+    CGFloat threeLineHeight = threeLineSize.height;
+    //    NSLog(@"计算高度方法1：text width = %f, text height = %f", size.width, size.height);
+    
+    CGFloat showHeight = 0;
+    if (isShowMore) {
+        showHeight = moreLineHeight;
+    } else {
+        showHeight = threeLineHeight;
+    }
+    
+    BOOL isHidden = NO;
+    if (!(threeLineHeight < moreLineHeight)) {
+        isHidden = YES;
+    }
+    if (isHidden) {
+        //不显示more按钮     +16
         showHeight += 16;
     } else {
-        //显示more按钮                  36
-        showHeight = 66 + 36;
+        //显示more按钮      +36
+        showHeight += 36;
     }
-    //状态发生变化 显示全部
-    if (isShowMore) {
-        showHeight = height + 36;
-    }
+    
     return showHeight;
 }
+
 @end
