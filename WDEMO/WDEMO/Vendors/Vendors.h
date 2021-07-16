6244520a5897184ecc588a4b5de701917769a11b
wuwenchao <>
2021-07-16 14:56:54 +0800

    文件位置修改

---
+++ WDEMO/WDEMO/Vendors/Vendors.h
@@ -0,0 +1,17 @@
+//
+//  Vendors.h
+//  WDEMO
+//
+//  Created by rrtv on 2021/7/16.
+//  Copyright © 2021 wwc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Vendors : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WDEMO/WDEMO/Vendors/Vendors.m
@@ -0,0 +1,13 @@
+//
+//  Vendors.m
+//  WDEMO
+//
+//  Created by rrtv on 2021/7/16.
+//  Copyright © 2021 wwc. All rights reserved.
+//
+
+#import "Vendors.h"
+
+@implementation Vendors
+
+@end
