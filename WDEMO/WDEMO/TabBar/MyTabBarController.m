e68e29b2d5de687bbe09a98fb5156eb32e47cd01
wuwenchao <>
2021-07-29 16:06:01 +0800

    UI

--- WDEMO/WDEMO/TabBar/MyTabBarController.m
+++ WDEMO/WDEMO/TabBar/MyTabBarController.m
@@ -7,7 +7,6 @@
 //
 
 #import "MyTabBarController.h"
-#import "MyTabBar.h"
 
 @interface MyTabBarController ()
 
@@ -18,22 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-//    MYTabBar *tabBar = [[MYTabBar alloc] init];
-//    tabBar.tabBarDelegate = self;
-//    
-//    [self setValue:tabBar forKeyPath:@"tabBar"];
-//    
 }
 
-//#pragma mark - MYTabBarDelegate代理方法
-//- (void)tabBarDidClickPlusButton:(MYTabBar *)tabBar {
-////    AddViewController *addVC= [[AddViewController alloc] init];
-////
-////    [self presentViewController:addVC animated:YES completion:nil];
-//}
-//
-////————————————————
-////版权声明：本文为CSDN博主「lwjok2007」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-////原文链接：https://blog.csdn.net/lwjok2007/article/details/51028886
- 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
 @end
