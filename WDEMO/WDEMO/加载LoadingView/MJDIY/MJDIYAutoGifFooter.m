6627239373c1eef1539b8d5d9c422703b563aa8a
wuwenchao <wuwenchao@rr.tv>
2022-04-20 11:14:23 +0800

    MJDIY 头 脚

--- WDEMO/WDEMO/加载LoadingView/MJDIY/MJDIYAutoGifFooter.m
+++ WDEMO/WDEMO/加载LoadingView/MJDIY/MJDIYAutoGifFooter.m
@@ -25,8 +25,10 @@
         //设置正在刷新状态的动画图片
         NSMutableArray * refreshingImages = [NSMutableArray array];
         for (NSUInteger i = 1; i<=3; i++) {
-            UIImage * image = [UIImage imageNamed:[NSString stringWithFormat:@"dropdown_loading_0%zd",i]];
-            [refreshingImages addObject:image];
+            UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"dropdown_loading_0%zd",i]];
+            if (image) {
+                [refreshingImages addObject:image];
+            }
         }
         [self setImages:refreshingImages forState:MJRefreshStateIdle];
         [self setImages:refreshingImages forState:MJRefreshStatePulling];
